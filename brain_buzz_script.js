6ec22f8be95dc1d3f9c6e1130f1441a73c8d9bf5
Humayun Shariar Himu <162119297+HumayunShariarHimu@users.noreply.github.com>
2024-10-17 06:15:04 +0600

    Add files via upload

---
+++ brain_buzz_script.js
@@ -0,0 +1,8 @@
+// let app = document.createElement("div");
+// app.setAttribute("id", "app");
+// document.body.prepend(app)
+
+// for (let i = 0; i < 7; i++) {
+// 	let span = document.createElement("span") 
+// 	app.prepend(span)
+// }
